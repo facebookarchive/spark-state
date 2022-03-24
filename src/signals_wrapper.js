c5762772ee6bab65f0ee455aafb210b44dbb93a4
carlaL0rena <72118248+carlaL0rena@users.noreply.github.com>
2022-03-24 16:00:50 +0000

    Sorted Participant Array (#12)

--- src/signals_wrapper.js
+++ src/signals_wrapper.js
@@ -3,6 +3,7 @@
  */
 
 const Reactive = require('Reactive')
+const Time = require('Time')
 
 /**
  * Returns a `ScalarSignal` object that can be modified via calls to the `increment` or `decrement` methods.
@@ -11,6 +12,12 @@ export async function Counter(startValue, signalName) {
   const source = Reactive.scalarSignalSource(signalName)
   source.set(startValue)
   const signal = source.signal;
+
+  signal.compareAndUpdateLocal = function(val) {
+    if (signal.pinLastValue() !== val) {
+      source.set(val)
+    }
+  }
 
   signal.setValueOnly = function (val) {
     source.set(val)
@@ -34,6 +41,8 @@ export async function Counter(startValue, signalName) {
     signal.setValueAndUpdate(signal.pinLastValue() - i)
   }
 
+  signal.setReceivedAllValues = function (val) {}
+
   return signal
 }
 
@@ -45,6 +54,12 @@ export async function String(startValue, signalName) {
   source.set(startValue);
   const signal = source.signal;
 
+  signal.compareAndUpdateLocal = function(val) {
+    if (signal.pinLastValue() !== val) {
+      source.set(val)
+    }
+  }
+
   signal.setValueOnly = function (val) {
     source.set(val)
   }
@@ -54,6 +69,8 @@ export async function String(startValue, signalName) {
     source.set(val)
     signal.updateState({ newValue: val, oldValue })
   }
+
+  signal.setReceivedAllValues = function (val) {}
 
   signal.set = signal.setValueAndUpdate;
 
@@ -68,6 +85,12 @@ export async function Scalar(startValue, signalName) {
   source.set(startValue)
   const signal = source.signal
 
+  signal.compareAndUpdateLocal = function(val) {
+    if (signal.pinLastValue() !== val) {
+      source.set(val)
+    }
+  }
+
   signal.setValueOnly = function (val) {
     source.set(val);
   }
@@ -78,7 +101,90 @@ export async function Scalar(startValue, signalName) {
     signal.updateState({newValue : val, oldValue})
   }
 
+  signal.setReceivedAllValues = function (val) {}
+
   signal.set = signal.setValueAndUpdate
 
   return signal
+}
+
+/**
+ * Returns an append-only `Array` object that can be modified via calls to the
+ * push method.
+ */
+export async function AppendOnlyArray(startValue, arrayName) {
+  let arrayWrapper = {}
+  let localArray = startValue
+  let localChangesQueue = []
+
+  let receivedAllValues = Reactive.boolSignalSource(`${arrayName}_receivedAllValues`)
+  receivedAllValues.set(false)
+
+  receivedAllValues.signal.onOn().subscribe(
+    event => {
+      if (event.newValue) {
+        if (localChangesQueue.length > 0) {
+          receivedAllValues.set(false)
+          arrayWrapper.updateState({
+            newValues: localChangesQueue.slice()
+          })
+          localArray.push(...localChangesQueue)
+          localChangesQueue = []
+          Time.setTimeout(() => {
+            receivedAllValues.set(true)
+          }, 50)
+        }
+      }
+  })
+
+  arrayWrapper.setReceivedAllValues = function (newValue) {
+    receivedAllValues.set(newValue)
+  }
+
+  arrayWrapper.receivedAllValuesSignal = receivedAllValues.signal
+
+  arrayWrapper.compareAndUpdateLocal = function (newArray) {
+    const currentLength = localArray.length
+    const newLength = newArray.length
+    let i
+    for (i = 0; i < currentLength && i < newLength; i++) {
+      localArray[i] = newArray[i]
+    }
+    if (newLength > currentLength) {
+      for (let j = i; j < newArray.length; j++) {
+        localArray.push(newArray[j])
+      }
+    } else if (newLength < currentLength && newLength != 0) {
+      localArray.splice(i, currentLength - newLength)
+    }
+  }
+
+  // Pushes the new value when the global array is synchronised with at least one peer
+  arrayWrapper.push = function (newValue) {
+    if (receivedAllValues.signal.pinLastValue()) {
+      receivedAllValues.set(false)
+      localArray.push(newValue)
+      arrayWrapper.updateState({newValues: [newValue]})
+      Time.setTimeout(() => {
+        receivedAllValues.set(true)
+      }, 50)
+    } else {
+      localChangesQueue.push(newValue)
+    }
+  }
+
+  // Returns a copy with the current elements of the array
+  arrayWrapper.getSnapshot = function (index) {
+    return localArray.slice()
+  }
+
+  arrayWrapper.length = function () {
+    return localArray.length
+  }
+
+  arrayWrapper.get = function (index) {
+    return localArray[index]
+  }
+
+  return arrayWrapper
 }
