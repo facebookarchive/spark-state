f7a887e075f0eb03568195eee9cf2809098cfa28
Xiaomeng Zhang <xiaomeng@fb.com>
2022-04-17 10:27:53 +0100

    Optimize periodic resync to send less messages
    Summary: 
    
    Test Plan: 
    
    Reviewers: 
    
    Subscribers: 
    
    Tasks: 
    
    Tags: 
    
    
    Differential Revision: https://phabricator.intern.facebook.com/D35707196

--- src/yjs_doc.js
+++ src/yjs_doc.js
@@ -18,6 +18,7 @@ function Uint8ArrayEquals(a, b) {
 }
 
 const NETWORK_ORIGIN = "network";
+const REFRESH_TARGETS_KEY = "t";
 
 export async function createYDoc(name) {
   const changesChannel = Multipeer.getBinaryMessageChannel(name + "_changes");
@@ -62,6 +63,31 @@ export async function createYDoc(name) {
     );
   }
 
+  async function resyncForActiveUnsyncedPeers() {
+    const myStateVector = stateVectors[myId];
+
+    // Get all peers which do not have the same state vector as self
+    const unsyncedPeers = Object.entries(stateVectors)
+      .filter(([_, stateVec]) => !Uint8ArrayEquals(myStateVector, stateVec))
+      .map(([peerId, _]) => peerId);
+
+    // Get all unsynced peers active status
+    const unsyncedActivePeers = await Promise.all(
+      unsyncedPeers.filter((id) =>
+        Participants.getParticipantById(id)
+          .catch((_) => false)
+          .then((p) => p.isActiveInSameEffect.pinLastValue())
+      )
+    );
+
+    // Send refresh message to all unsynced active peers
+    if (unsyncedActivePeers.length !== 0) {
+      refreshChannel.sendMessage({
+        [REFRESH_TARGETS_KEY]: unsyncedActivePeers,
+      });
+    }
+  }
+
   /* End of Utitily Functions */
 
   // when ydoc is changed, update all peers for my latest states.
@@ -76,14 +102,21 @@ export async function createYDoc(name) {
   });
 
   // Send my latest state vector upon peer request
-  refreshChannel.onMessage.subscribe((node) => {
+  refreshChannel.onMessage.subscribe((msg) => {
+    // If refresh message is not targetting for self,
+    // then return without responding.
+    if (msg[REFRESH_TARGETS_KEY] && !msg[REFRESH_TARGETS_KEY].includes(myId)) {
+      return;
+    }
+
+    // Send state vector back upon request.
     stateVectorsChannel.sendMessage(
       encoding.encodeMessage({ s: myId }, stateVectors[myId])
     );
   });
 
   // Request all peers to send their latest state vector
-  refreshChannel.sendMessage(myId);
+  refreshChannel.sendMessage({});
 
   // handle new changes coming from other peers
   changesChannel.onMessage.subscribe((m) => {
@@ -117,7 +150,7 @@ export async function createYDoc(name) {
   // When remote peer receives this, it will send an update message if needed.
   // This is necessary because Multipeer API is a lossy channel.
   // This periodically resync is needed to recover lost update.
-  Time.setInterval(broadcastStateVectorToPeers, 2000);
+  Time.setInterval(resyncForActiveUnsyncedPeers, 3000);
 
   return doc;
 }
